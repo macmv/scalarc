f0ec7ae673b81d95b902353b5f8981db45d92360
Neil Macneale V <neil.macneale.v@gmail.com>
2024-01-15 15:02:46 -0800

    Added conversion functions, and things compile now

--- scalarc-syntax/src/node.rs
+++ scalarc-syntax/src/node.rs
@@ -1,5 +1,15 @@
+use rowan::Language;
+use scalarc_parser::SyntaxKind;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Scala {}
+impl Language for Scala {
+  type Kind = SyntaxKind;
+
+  fn kind_from_raw(raw: rowan::SyntaxKind) -> SyntaxKind { SyntaxKind::from(raw.0) }
+
+  fn kind_to_raw(kind: SyntaxKind) -> rowan::SyntaxKind { rowan::SyntaxKind(kind.into()) }
+}
 
 pub type SyntaxNode = rowan::SyntaxNode<Scala>;
 pub type SyntaxToken = rowan::SyntaxToken<Scala>;
