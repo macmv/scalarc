a2730f093e9951c6eb906de21c080cdd7e97c140
Neil Macneale V <neil.macneale.v@gmail.com>
2024-01-15 16:03:23 -0800

    Remove unused KIND_SRC fields

--- scalarc-build/src/ast_src.rs
+++ scalarc-build/src/ast_src.rs
@@ -1,15 +1,12 @@
 //! Defines input for code generation process.
 
 pub(crate) struct KindsSrc<'a> {
-  pub(crate) punct:               &'a [(&'a str, &'a str)],
-  pub(crate) keywords:            &'a [&'a str],
-  pub(crate) contextual_keywords: &'a [&'a str],
-  pub(crate) literals:            &'a [&'a str],
-  pub(crate) tokens:              &'a [&'a str],
+  pub(crate) punct:    &'a [(&'a str, &'a str)],
+  pub(crate) literals: &'a [&'a str],
 }
 
 pub(crate) const KINDS_SRC: KindsSrc<'_> = KindsSrc {
-  punct:               &[
+  punct:    &[
     ("-", "MINUS"),
     ("+", "PLUS"),
     ("'{'", "OPEN_CURLY"),
@@ -33,65 +30,7 @@ pub(crate) const KINDS_SRC: KindsSrc<'_> = KindsSrc {
     ("~", "TILDE"),
     ("_", "UNDERSCORE"),
   ],
-  keywords:            &[
-    // control flow
-    "if",
-    "match",
-    "case",
-    "while",
-    "try",
-    "do",
-    "for",
-    "throw",
-    "return",
-    // exprs
-    "this",
-    "super",
-    "type",
-    "true",
-    "false",
-    "null",
-    // statements
-    "val",
-    "var",
-    "implicit",
-    // items
-    "class",
-    "trait",
-    "object",
-    "package",
-    "extends",
-    "with",
-    "def",
-    "abstract",
-    "final",
-    "protected",
-    "private",
-    "sealed",
-  ],
-  contextual_keywords: &[
-    "auto",
-    "builtin",
-    "default",
-    "existential",
-    "union",
-    "raw",
-    "macro_rules",
-    "yeet",
-    "offset_of",
-    "asm",
-    "format_args",
-  ],
-  literals:            &[
-    "INT_NUMBER",
-    "FLOAT_NUMBER",
-    "CHAR",
-    "BYTE",
-    "STRING",
-    "BYTE_STRING",
-    "C_STRING",
-  ],
-  tokens:              &["ERROR", "IDENT", "WHITESPACE", "LIFETIME_IDENT", "COMMENT", "SHEBANG"],
+  literals: &["INT_NUMBER", "FLOAT_NUMBER", "CHAR", "BYTE", "STRING", "BYTE_STRING", "C_STRING"],
 };
 
 #[derive(Default, Debug)]
