39cbc9643f9f4a3f1825a347bdfbb293e55ba3d5
Neil Macneale V <neil.macneale.v@gmail.com>
2024-05-01 20:51:19 -0700

    Simplify sample

--- sample/src/main/scala/Main.scala
+++ sample/src/main/scala/Main.scala
@@ -1,39 +1,13 @@
 
 
-
-object Main extends App {
-  println("Hello, World!")
-
-  def hello(x: Int): Int = {
-    y + 0
-
-    val y = 3
-    y + 2
-
-    x + 1
-  }
-
-  y + 3
-
-  val x = 1
-}
-
 class Foo(a: String) {
   println("Hello, World!", a)
 
   val b = 3
 
-  a
-  b
+  def foo(c: Int) = {
+    a + b + c
+  }
 
   Foo
-  Main
 }
-
-val x = 3
-
-x
-
-
-Main
-Foo
