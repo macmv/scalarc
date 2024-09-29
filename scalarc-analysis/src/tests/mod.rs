3eca3d239d81a1228aad587383d648af2c252b8d
Neil Macneale V <neil.macneale.v@gmail.com>
2024-09-29 14:42:44 -0700

    Include ministd in the analysis tests

--- scalarc-analysis/src/tests/mod.rs
+++ scalarc-analysis/src/tests/mod.rs
@@ -1,5 +1,7 @@
+use std::{path::PathBuf, sync::Arc};
+
 use la_arena::Arena;
-use scalarc_source::{FileId, SourceDatabase, TargetData, Workspace};
+use scalarc_source::{FileId, SourceDatabase, SourceRoot, TargetData};
 
 use crate::{completion::completions, database::RootDatabase};
 
@@ -14,18 +16,35 @@ fn completions_for(src: &str) -> Vec<String> {
 
   let mut db = RootDatabase::default();
   let file = FileId::temp_new();
-  let mut source_roots = Arena::new();
-  let source =
-    source_roots.alloc(scalarc_source::SourceRoot { path: "/".into(), sources: vec![file] });
-  let mut targets = Arena::new();
-  targets.alloc(TargetData {
-    dependencies: vec![],
-    bsp_id:       "file:///".try_into().unwrap(),
-    source_roots: vec![source],
-  });
-  db.set_workspace(Workspace { root: "/".into(), targets, source_roots }.into());
-  db.set_file_source_root(file, Some(source));
   db.set_file_text(file, real_src.into());
+
+  let std = FileId::new_raw(1);
+  db.set_file_text(std, include_str!("../../../scalarc-hir/src/tests/ministd.scala").into());
+
+  // Build a workspace with two targets: one for std, and one for this test.
+  let mut targets = Arena::new();
+  let mut source_roots = Arena::new();
+
+  let std_source = source_roots.alloc(SourceRoot { path: PathBuf::new(), sources: vec![std] });
+  let test_source = source_roots.alloc(SourceRoot { path: PathBuf::new(), sources: vec![file] });
+
+  let std_target = targets.alloc(TargetData {
+    dependencies: vec![],
+    bsp_id:       "file:///std".parse().unwrap(),
+    source_roots: vec![std_source],
+  });
+
+  targets.alloc(TargetData {
+    dependencies: vec![std_target],
+    bsp_id:       "file:///test".parse().unwrap(),
+    source_roots: vec![test_source],
+  });
+
+  db.set_file_source_root(std, Some(std_source));
+  db.set_file_source_root(file, Some(test_source));
+
+  let workspace = scalarc_source::Workspace { root: Default::default(), targets, source_roots };
+  db.set_workspace(Arc::new(workspace));
 
   simple_completions(&db, crate::FileLocation { file, index: cursor.into() })
 }
@@ -43,7 +62,7 @@ fn check_completions() {
         }
       "#,
     ),
-    vec!["Foo", "z", "y", "x"]
+    vec!["Foo", "Int", "z", "y", "x"]
   );
 
   assert_eq!(
@@ -57,7 +76,7 @@ fn check_completions() {
         }
       "#,
     ),
-    vec!["Foo", "y", "x"]
+    vec!["Foo", "Int", "y", "x"]
   );
 
   assert_eq!(
@@ -70,6 +89,6 @@ fn check_completions() {
         }
       "#,
     ),
-    vec!["Foo", "y", "x"]
+    vec!["Foo", "Int", "y", "x"]
   );
 }
