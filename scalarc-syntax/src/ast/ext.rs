7dfdc4faa31816864d9dc92675b3624891964623
Neil Macneale V <neil.macneale.v@gmail.com>
2024-10-02 08:24:56 -0700

    Add from/to accessor for import selectors

--- scalarc-syntax/src/ast/ext.rs
+++ scalarc-syntax/src/ast/ext.rs
@@ -1,3 +1,5 @@
+use scalarc_parser::T;
+
 use super::support;
 use crate::ast;
 
@@ -10,4 +12,13 @@ impl ast::IfExpr {
   pub fn cond(&self) -> Option<ast::Expr> { support::children(&self.syntax).next() }
   pub fn then(&self) -> Option<ast::Expr> { support::children(&self.syntax).nth(1) }
   pub fn els(&self) -> Option<ast::Expr> { support::children(&self.syntax).nth(2) }
+}
+
+impl ast::ImportSelectorRename {
+  pub fn from(&self) -> Option<ast::SyntaxToken> {
+    support::token_children(&self.syntax, T![ident]).next()
+  }
+  pub fn to(&self) -> Option<ast::SyntaxToken> {
+    support::token_children(&self.syntax, T![ident]).nth(1)
+  }
 }
