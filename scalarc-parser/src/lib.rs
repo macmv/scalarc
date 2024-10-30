67b449cdd0abb8c2b4dc95569cb30a6a26d0d9fd
Neil Macneale V <neil.macneale.v@gmail.com>
2024-10-29 22:21:16 -0700

    Factor out event processing to its own crate

--- scalarc-parser/src/lib.rs
+++ scalarc-parser/src/lib.rs
@@ -6,7 +6,7 @@ mod syntax_kind;
 mod tests;
 mod token;
 
-use std::{mem, ops::Range};
+use std::ops::Range;
 
 use drop_bomb::DropBomb;
 pub use syntax_kind::SyntaxKind;
@@ -145,71 +145,13 @@ fn token_to_kind(token: Token, s: &str) -> SyntaxKind {
   }
 }
 
-/// `Parser` produces a flat list of `Event`s.
-/// They are converted to a tree-structure in
-/// a separate pass, via `TreeBuilder`.
-#[derive(Debug)]
-pub enum Event {
-  /// This event signifies the start of the node.
-  /// It should be either abandoned (in which case the
-  /// `kind` is `TOMBSTONE`, and the event is ignored),
-  /// or completed via a `Finish` event.
-  ///
-  /// All tokens between a `Start` and a `Finish` would
-  /// become the children of the respective node.
-  ///
-  /// For left-recursive syntactic constructs, the parser produces
-  /// a child node before it sees a parent. `forward_parent`
-  /// saves the position of current event's parent.
-  ///
-  /// Consider this path
-  ///
-  /// foo::bar
-  ///
-  /// The events for it would look like this:
-  ///
-  /// ```text
-  /// START(PATH) IDENT('foo') FINISH START(PATH) T![::] IDENT('bar') FINISH
-  ///       |                          /\
-  ///       |                          |
-  ///       +------forward-parent------+
-  /// ```
-  ///
-  /// And the tree would look like this
-  ///
-  /// ```text
-  ///    +--PATH---------+
-  ///    |   |           |
-  ///    |   |           |
-  ///    |  '::'       'bar'
-  ///    |
-  ///   PATH
-  ///    |
-  ///   'foo'
-  /// ```
-  ///
-  /// See also `CompletedMarker::precede`.
-  Start {
-    kind:           SyntaxKind,
-    forward_parent: Option<u32>,
-  },
+pub type Event = scalarc_parser_base::Event<SyntaxKind>;
 
-  /// Complete the previous `Start` event
-  Finish,
-
-  /// Produce a single leaf-element.
-  Token {
-    kind: SyntaxKind,
-    len:  usize,
-  },
-
-  Error {
-    msg: String,
-  },
+impl scalarc_parser_base::SyntaxKind for SyntaxKind {
+  const TOMBSTONE: Self = SyntaxKind::TOMBSTONE;
 }
-impl Event {
-  pub fn tombstone() -> Self { Event::Token { kind: SyntaxKind::TOMBSTONE, len: 0 } }
-}
+
+pub use scalarc_parser_base::process_events;
 
 impl EntryPoint {
   pub fn parse<'a>(&'a self, lexer: &'a mut Lexer<'a>) -> Vec<Event> {
@@ -492,55 +434,4 @@ impl CompletedMarker {
     }
     new_pos
   }
-}
-
-// Removes all the forward_parents. TODO: Produce a new Output type or something
-// to avoid exposing Event.
-pub fn process_events(events: &mut [Event]) -> Vec<Event> {
-  let mut out = vec![];
-  let mut forward_parents = Vec::new();
-
-  for i in 0..events.len() {
-    match mem::replace(&mut events[i], Event::tombstone()) {
-      Event::Start { kind, forward_parent } => {
-        // For events[A, B, C], B is A's forward_parent, C is B's forward_parent,
-        // in the normal control flow, the parent-child relation: `A -> B -> C`,
-        // while with the magic forward_parent, it writes: `C <- B <- A`.
-
-        // append `A` into parents.
-        forward_parents.push(kind);
-        let mut idx = i;
-        let mut fp = forward_parent;
-        while let Some(fwd) = fp {
-          idx += fwd as usize;
-          // append `A`'s forward_parent `B`
-          fp = match mem::replace(&mut events[idx], Event::tombstone()) {
-            Event::Start { kind, forward_parent } => {
-              forward_parents.push(kind);
-              forward_parent
-            }
-            _ => unreachable!(),
-          };
-          // append `B`'s forward_parent `C` in the next stage.
-        }
-
-        for kind in forward_parents.drain(..).rev() {
-          if kind != SyntaxKind::TOMBSTONE {
-            out.push(Event::Start { kind, forward_parent: None });
-          }
-        }
-      }
-      Event::Finish => {
-        out.push(Event::Finish);
-      }
-      Event::Token { kind, len } => {
-        if kind != SyntaxKind::TOMBSTONE {
-          out.push(Event::Token { kind, len });
-        }
-      }
-      Event::Error { msg } => out.push(Event::Error { msg }),
-    }
-  }
-
-  out
 }
